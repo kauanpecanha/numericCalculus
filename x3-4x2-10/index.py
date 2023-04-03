92df50f9aa0d1bbd37cddd96f67db9d74234bfe9
Kauan Peçanha <kauanpecanha1@gmail.com>
2023-04-03 15:56:29 -0300

    update

---
+++ x3-4x2-10/index.py
@@ -0,0 +1,45 @@
+
+# método da bisseção(ou valor intermediário?)
+
+a = float(input("Entre com o primeiro número do intervalo: "))
+b = float(input("Entre com o segundo número do intervalo: "))
+epsilon = float(input('Entre com a tolerância desejada: '))
+tol = 0
+x = 0
+iteration = 0
+
+def setXn(k, l):
+    return ((k+l)/(2))
+
+def setFunc(x):
+    return (((x)**3) + ((4)*(x**2)) - (10))
+
+def setTol(i, j):
+    return (abs(j - i))
+
+tol = setTol(a, b)
+
+while(tol>epsilon):
+    
+    x = setXn(a, b)
+
+    f_a = setFunc(a)
+    f_b = setFunc(b)
+    f_x = setFunc(x)
+
+    if((f_a)*(f_x)<0):
+        b = x
+    else:
+        a = x
+
+
+    # print(f'{f_a}, {f_b}, {f_x}')
+    
+    tol = setTol(a, b)
+
+    iteration+=1
+
+    x = setXn(a, b)
+
+print(f'Range of possible solutions: [{a:.4f}, {b:.4f}], near {x:.4f}')
+print(f'Total of iterations: {iteration}')
