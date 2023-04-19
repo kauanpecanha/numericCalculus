8551b5777eb7b7514d5ccd48bb1b725c4fabed3c
Kauan Peçanha <kauanpecanha1@gmail.com>
2023-04-19 11:07:28 -0300

    estimating iterations code update

---
+++ bissectionMethod/iterationsEstimative/index.py
@@ -0,0 +1,14 @@
+from math import log, ceil
+
+# bissection method
+
+a = float(input("Entre com o primeiro número do intervalo: "))
+b = float(input("Entre com o segundo número do intervalo: "))
+epsilon = float(input('Entre com a tolerância desejada: '))
+tol = 0
+x = 0
+aproxIterationsNumber = 0
+
+aproxIterationsNumber = ((log(b-a) - log(epsilon))/(log(2)))
+
+print(f'Aproximate number of iterations: {ceil(aproxIterationsNumber)}')
